f47329988c5fc1a9f8228619cd2e9b07ae3b6492
kawtar <kawtar.nouara@gmail.com>
2022-11-03 15:43:11 +0100

    unquit app when close button clicked

--- main.js
+++ main.js
@@ -57,6 +57,7 @@ app.on('open-url', function (ev, url) {
 });
 
 app.on('ready', async () => {
+    app.isQuiting  = false;
     electron.powerMonitor.on('lock-screen', () => {
         if(win){
             win.webContents.send('screen-lock-change', 'lock');
@@ -125,13 +126,24 @@ app.on('ready', async () => {
             mainurl = null;
         }
     });
+    win.on('close', event => {
+        if(app.isQuiting){
+            return;
+        }
+        event.preventDefault();
+        win.hide();
+    })
 });
 
 
 app.on('before-quit', () => {
-    BrowserWindow.getAllWindows().map(window => {
+   /* BrowserWindow.getAllWindows().map(window => {
         window.destroy();
-    });
+    });*/
+});
+
+app.on('quit', (ev) => {
+    app.isQuiting  = false;
 });
 
 // Quit when all windows are closed.
@@ -151,6 +163,8 @@ app.on('activate', async () => {
     if (win === null) {
         win = await createMainWindow(dev)
         new Badge(win, {});
+    }else{
+        win.show();
     }
 });
 
