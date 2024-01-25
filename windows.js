8707f49ee79b596ee535d3f91d388f75a7ba5cc9
kawtarnouara <k.nouara@piman-group.fr>
2024-01-25 17:20:53 +0100

    keep window position, height, width

--- windows.js
+++ windows.js
@@ -7,7 +7,7 @@ const urlM = require('url');
 const {autoUpdater} = require("electron-updater");
 const { dialog } = require('electron')
 const Sentry = require("@sentry/electron");
-
+let windowInfos;
 const remoteMain = require("@electron/remote/main");
 function handleError(err) {
     console.error(err);
@@ -77,6 +77,9 @@ exports.createWindow =  function(i18n, dev = true) {
                 const mainScreen = electronScreen.getPrimaryDisplay();
                 const { width, height } = mainScreen.workAreaSize;
                 new_win.on('blur', () => {
+                    const windowSize = new_win.getSize();
+                    const windowPosition = new_win.getPosition();
+                    windowInfos = {width: windowSize[0], height: windowSize[1], x: windowPosition[0], y: windowPosition[1]}
                     const smallWindowWidth = 350;
                     const smallWindowHeight = 350;
                     new_win.setMinimumSize(smallWindowWidth, smallWindowHeight);
@@ -90,11 +93,9 @@ exports.createWindow =  function(i18n, dev = true) {
                 });
                 const enlarge = () => {
                     new_win.setMinimumSize(500, 500);
-                    new_win.setSize(1300, 800);
+                    new_win.setSize(windowInfos.width, windowInfos.height);
                     new_win.setAlwaysOnTop(false);
-                    const x = Math.floor((width - 1300) / 2);
-                    const y = Math.floor((height - 800) / 2);
-                    new_win.setPosition(x, y);
+                    new_win.setPosition(windowInfos.x, windowInfos.y);
                     new_win.webContents.send('smaller-room', false);
                 };
                 new_win.on('focus', () => {
