9095190e6f466c7b058c6bd6e865b418655275b4
Kawtar Windows <kawtar.nouara@gmail.com>
2024-11-04 10:45:24 +0100

    handle translation + jwt token

--- main.js
+++ main.js
@@ -348,7 +348,7 @@ ipcMain.on('notification-click', (event) => {
     }
 });
 
-ipcMain.on('language-change', (event, lang) => {
+ipcMain.on('change-language', (event, lang) => {
     changeLang(i18n, lang, win);
 });
 
